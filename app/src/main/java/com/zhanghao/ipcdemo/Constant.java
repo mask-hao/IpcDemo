91e9ae541c6e99422bad6a89cd5ba714ee23c761
maskhao <281079145@qq.com>
2017-06-27 18:47:21 +0800

    add aidl

---
+++ app/src/main/java/com/zhanghao/ipcdemo/Constant.java
@@ -0,0 +1,11 @@
+package com.zhanghao.ipcdemo;
+
+/**
+ * Created by zhanghao on 2017/6/27.
+ */
+
+public class Constant {
+    public static final int MSG_FROM_CLIENT =100;
+    public static final int MSG_FROM_REMOTE_SERVICE =101;
+    public static final int MSG_NEW_BOOK_ARRIVED=102;
+}
